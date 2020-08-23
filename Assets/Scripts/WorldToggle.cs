89e9b2f60fea313a4627bf78c8df94a412815f57
Allen Bao <allenbao01@gmail.com>
2020-08-22 22:33:02 -0700

    VERY FAULTY VERSIONgit status! New assets also added.

---
+++ Assets/Scripts/WorldToggle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldToggle : MonoBehaviour
+{
+    //access ripple VFX
+    public ParticleSystem rippleFX;
+
+    private Animator animator;
+    private bool color = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    //trigger animation of mask (changes world)
+    public void Toggle()
+    {
+        color = !color;
+        animator.SetBool("Color", color);
+        //instantiate the ripplefx
+        GameObject rfx = Instantiate(rippleFX.gameObject, GetComponent<Transform>().position, Quaternion.identity);
+    }
+}
